d210520f192db7413a731e5bca5aa21b624b7479
fteychene <francois.teychene@gmail.com>
2023-04-05 17:14:23 +0200

    feat: Remove dependency from gateway-test to logger interceptor by adding a test interceptor in project test for testing this case

--- gateway-test/src/test/java/io/conduktor/gateway/integration/interceptor/LoggerInterceptorPluginIntegrationTest.java
+++ gateway-test/src/test/java/io/conduktor/gateway/integration/interceptor/LoggerInterceptorPluginIntegrationTest.java
@@ -20,10 +20,7 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.protocol.ApiKeys;
-import org.apache.kafka.common.requests.AbstractRequestResponse;
-import org.apache.kafka.common.requests.AbstractResponse;
-import org.apache.kafka.common.requests.FetchResponse;
-import org.apache.kafka.common.requests.ProduceRequest;
+import org.apache.kafka.common.requests.*;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.core.Appender;
 import org.apache.logging.log4j.core.LoggerContext;
@@ -51,19 +48,16 @@ public class LoggerInterceptorPluginIntegrationTest extends BaseGatewayIntegrati
     public static final String TEST_CLIENT_001 = "testClient001";
     public static final String SOME_KEY = "someKey";
     public static final String SOME_VALUE = "someValue";
-    public static final String LOGGERINTERCEPTOR_PACKAGE = "io.conduktor.example.loggerinterceptor";
+    public static final String LOGGERINTERCEPTOR_PACKAGE = "io.conduktor.gateway.integration.interceptor";
 
     @Test
     public void testLoadsInterceptors() {
-
-        assertThat(getInterceptorPoolService().getAllInterceptors(ProduceRequest.class).size())
+        assertThat(getInterceptorPoolService().getAllInterceptors(FetchRequest.class).size())
                 .isEqualTo(2);
-        assertThat(getInterceptorPoolService().getAllInterceptors(FetchResponse.class).size())
-                .isEqualTo(3);
         assertThat(getInterceptorPoolService().getAllInterceptors(AbstractRequestResponse.class).size())
                 .isEqualTo(1);
         assertThat(getInterceptorPoolService().getAllInterceptors(AbstractResponse.class).size())
-                .isEqualTo(2);
+                .isEqualTo(1);
     }
 
     @Test
@@ -85,14 +79,6 @@ public class LoggerInterceptorPluginIntegrationTest extends BaseGatewayIntegrati
         // global interceptor
         assertThat(new String(logStream.toByteArray())
                 .contains("Hello there, a class org.apache.kafka.common.requests.ProduceResponse was sent/received"))
-                .isEqualTo(true);
-        // response interceptor
-        assertThat(new String(logStream.toByteArray())
-                .contains("A RESPONSE was sent"))
-                .isEqualTo(true);
-        // produce interceptor
-        assertThat(new String(logStream.toByteArray())
-                .contains("Produce was called with version: " + ApiKeys.PRODUCE.latestVersion()))
                 .isEqualTo(true);
     }
 
@@ -117,44 +103,10 @@ public class LoggerInterceptorPluginIntegrationTest extends BaseGatewayIntegrati
         assertThat(new String(logStream.toByteArray())
                 .contains("Hello there, a class org.apache.kafka.common.requests.LeaveGroupResponse was sent/received"))
                 .isEqualTo(true);
-        // response interceptor
-        assertThat(new String(logStream.toByteArray())
-                .contains("A RESPONSE was sent"))
-                .isEqualTo(true);
         // fetch interceptor
         assertThat(new String(logStream.toByteArray())
                 .contains("Fetch was requested from localhost"))
                 .isEqualTo(true);
-        assertThat(new String(logStream.toByteArray())
-                .contains("Fetch from client localhost was responded to"))
-                .isEqualTo(true);
-    }
-
-    @Test
-    public void addsHeaderOnProduce() throws ExecutionException, InterruptedException {
-        var clientTopic = createTopic(clientFactory.kafkaAdmin(), 1, (short) 1);
-
-        clientFactory.addGatewayPropertyOverride(ProducerConfig.CLIENT_ID_CONFIG, TEST_CLIENT_001);
-        try (var gatewayProducer = clientFactory.gatewayProducer()) {
-            ProducerRecord<String, String> record1 = new ProducerRecord(clientTopic, SOME_KEY, SOME_VALUE);
-            gatewayProducer.send(record1).get();
-        }
-
-        var consumedRecords = new ArrayList<ConsumerRecord>();
-        try (var gatewayConsumer = clientFactory.gatewayConsumer(getGroup())) {
-            gatewayConsumer.subscribe(Collections.singletonList(clientTopic));
-            var startTime = System.currentTimeMillis();
-            while (consumedRecords.size() == 0 && System.currentTimeMillis() < startTime + 20_000L) {
-                var records = gatewayConsumer.poll(Duration.ofMillis(100));
-                records.forEach(record -> consumedRecords.add(record));
-            }
-        }
-
-        assertThat(consumedRecords.size())
-                .isEqualTo(1);
-        assertThat(consumedRecords.get(0).headers().lastHeader("fromClient").value())
-                .isEqualTo(TEST_CLIENT_001.getBytes());
-
     }
 
     void addAppender(final Writer writer, final String writerName) {
